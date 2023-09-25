908af415f5e7af5b3a95c840ba2d227098fd80e4
ArunDeveloper08 <128569214+ArunDeveloper08@users.noreply.github.com>
2023-09-25 12:43:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 app.use('/', Router) 
 Connection();
- const PORT=8000;
+ const PORT=process.env.PORT || 8000;
  
 app.listen(PORT,()=> console.log(`server is running Sucessfully ${PORT}`)
 ) 
